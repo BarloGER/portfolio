9cb986dba1c5d2516d26fc024f6e1d9457eb6b77
Mike Barlozewski <108719328+BarloGER@users.noreply.github.com>
2025-01-07 13:57:34 +0100

    feat: add projects and contact sections

--- script.js
+++ script.js
@@ -1,75 +1,4 @@
 /***************************************************
- * Change Language
- ***************************************************/
-let currentLanguage = "de";
-
-function updateTexts(language) {
-  const elements = document.querySelectorAll("[data-i18n]");
-  elements.forEach((el) => {
-    const translationKey = el.getAttribute("data-i18n").split(".");
-    let text = translations[language];
-    translationKey.forEach((key) => {
-      text = text[key];
-    });
-    el.textContent = text;
-  });
-}
-
-const languageToggle = document.getElementById("btn-language-toggle");
-const currentFlag = document.getElementById("current-flag");
-
-languageToggle.addEventListener("click", () => {
-  const newLanguage = currentLanguage === "de" ? "en" : "de";
-
-  currentLanguage = newLanguage;
-
-  currentFlag.src = `assets/flag-${newLanguage}.svg`;
-  languageToggle.setAttribute("data-lang", newLanguage);
-  updateTexts(newLanguage);
-});
-
-// Load default language de on start
-document.addEventListener("DOMContentLoaded", () => {
-  updateTexts(currentLanguage);
-});
-
-/***************************************************
- * THEME TOGGLE
- ***************************************************/
-const themeToggleBtn = document.getElementById("btn-theme-toggle");
-
-themeToggleBtn.addEventListener("click", () => {
-  const htmlEl = document.documentElement;
-
-  if (htmlEl.classList.contains("theme-light")) {
-    htmlEl.classList.remove("theme-light");
-    htmlEl.classList.add("theme-dark");
-    themeToggleBtn.querySelector("img").src = "assets/moon.svg";
-  } else {
-    htmlEl.classList.remove("theme-dark");
-    htmlEl.classList.add("theme-light");
-    themeToggleBtn.querySelector("img").src = "assets/sun.svg";
-  }
-});
-
-let aboutLinks = document.getElementsByClassName("about__links");
-let aboutContents = document.getElementsByClassName("about__contents");
-
-function openContentTab(contentName) {
-  for (aboutLink of aboutLinks) {
-    aboutLink.classList.remove("about__links--active");
-  }
-
-  for (aboutContent of aboutContents) {
-    aboutContent.classList.remove("about__contents--active");
-  }
-
-  event.currentTarget.classList.add("about__links--active");
-
-  document.getElementById(contentName).classList.add("about__contents--active");
-}
-
-/***************************************************
  * Translations
  ***************************************************/
 
@@ -117,20 +46,19 @@ const translations = {
     },
     projects: {
       title: "My Projects",
-      detailButton: "Open Project",
+      technology: "Technology",
     },
     contact: {
       title: "Contact Me",
-      nameLabel: "Name:",
-      emailLabel: "Email:",
-      messageLabel: "Message:",
       submit: "Send",
+      successMessage: "Message sent successfully",
+      errorMessage: "Error while sending message",
     },
   },
   de: {
     hero: {
       nav: {
-        home: "Start",
+        home: "Home",
         about: "Über mich",
         projects: "Projekte",
         contact: "Kontakt",
@@ -153,7 +81,7 @@ const translations = {
           text: "Derzeit arbeite ich daran, in die Softwarebranche einzusteigen. Dafür vertiefe ich meine Kenntnisse und lerne kontinuierlich neue Fertigkeiten. Zudem arbeite ich an spannenden Projekten, die man sich entweder bereits jetzt oder in Kürze im Abschnitt 'Projekte' ansehen kann.",
         },
         future: {
-          title: "Future",
+          title: "Zukunft",
           text: "Für die Zukunft plane ich, meinen Leistungswillen einzusetzen, um mich zu einem vielseitigen und erfahrenen Senior Developer weiterzuentwickeln.",
         },
       },
@@ -170,14 +98,150 @@ const translations = {
     },
     projects: {
       title: "Meine Projekte",
-      detailButton: "Zum Projekt",
+      technology: "Technologie",
     },
     contact: {
       title: "Kontakt",
-      nameLabel: "Name:",
-      emailLabel: "E-Mail:",
-      messageLabel: "Nachricht:",
       submit: "Senden",
+      successMessage: "Nachricht erfolgreich versendet",
+      errorMessage: "Fehler beim versenden der Nachricht.",
     },
   },
 };
+
+/***************************************************
+ * Change Language
+ ***************************************************/
+let currentLanguage = "de";
+
+function updateTexts(language) {
+  const elements = document.querySelectorAll("[data-i18n]");
+  elements.forEach((el) => {
+    const translationKey = el.getAttribute("data-i18n").split(".");
+    let text = translations[language];
+    translationKey.forEach((key) => {
+      text = text[key];
+    });
+    el.textContent = text;
+  });
+}
+
+const languageToggle = document.getElementById("btn-language-toggle");
+const currentFlag = document.getElementById("current-flag");
+
+languageToggle.addEventListener("click", () => {
+  const newLanguage = currentLanguage === "de" ? "en" : "de";
+
+  currentLanguage = newLanguage;
+
+  currentFlag.src = `assets/flag-${newLanguage}.svg`;
+  languageToggle.setAttribute("data-lang", newLanguage);
+  updateTexts(newLanguage);
+});
+
+// Load default language de on start
+document.addEventListener("DOMContentLoaded", () => {
+  const userPrefersDark = window.matchMedia(
+    "(prefers-color-scheme: dark)"
+  ).matches;
+
+  const htmlEl = document.documentElement;
+  const themeToggleBtn = document.getElementById("btn-theme-toggle");
+
+  // Set the initial theme based on user preference
+  if (userPrefersDark) {
+    htmlEl.classList.remove("theme-light");
+    htmlEl.classList.add("theme-dark");
+    themeToggleBtn.querySelector("img").src = "assets/moon.svg"; // Adjust to your dark theme icon
+  } else {
+    htmlEl.classList.remove("theme-dark");
+    htmlEl.classList.add("theme-light");
+    themeToggleBtn.querySelector("img").src = "assets/sun.svg"; // Adjust to your light theme icon
+  }
+
+  updateTexts(currentLanguage);
+});
+
+/***************************************************
+ * THEME TOGGLE
+ ***************************************************/
+const themeToggleBtn = document.getElementById("btn-theme-toggle");
+
+themeToggleBtn.addEventListener("click", () => {
+  const htmlEl = document.documentElement;
+
+  if (htmlEl.classList.contains("theme-light")) {
+    htmlEl.classList.remove("theme-light");
+    htmlEl.classList.add("theme-dark");
+    themeToggleBtn.querySelector("img").src = "assets/moon.svg";
+  } else {
+    htmlEl.classList.remove("theme-dark");
+    htmlEl.classList.add("theme-light");
+    themeToggleBtn.querySelector("img").src = "assets/sun.svg";
+  }
+});
+
+let aboutLinks = document.getElementsByClassName("about__links");
+let aboutContents = document.getElementsByClassName("about__contents");
+
+function openContentTab(contentName) {
+  for (aboutLink of aboutLinks) {
+    aboutLink.classList.remove("about__links--active");
+  }
+
+  for (aboutContent of aboutContents) {
+    aboutContent.classList.remove("about__contents--active");
+  }
+
+  event.currentTarget.classList.add("about__links--active");
+
+  document.getElementById(contentName).classList.add("about__contents--active");
+}
+
+/***************************************************
+ * SUBMIT TO GOOGLE SHEET
+ ***************************************************/
+
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycbzYnts9pQNPIf9mMtwegVRLE6hUvHQt2TmFjZFZM-rt4-HiVP2VJA97aF1u66wwlh2R/exec";
+const form = document.forms["submit-to-google-sheet"];
+const msg = document.getElementById("msg");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => {
+      msg.innerHTML =
+        currentLanguage === "de"
+          ? translations.de.contact.successMessage
+          : translations.en.contact.successMessage;
+      setTimeout(function () {
+        msg.innerHTML = "";
+      }, 5000);
+      form.reset();
+    })
+    .catch((error) => {
+      console.error("Error!", error.message);
+      msg.innerHTML =
+        currentLanguage === "de"
+          ? translations.de.contact.errorMessage
+          : translations.en.contact.errorMessage;
+      setTimeout(function () {
+        msg.innerHTML = "";
+      }, 5000);
+    });
+});
+
+/***************************************************
+ * MENU TOGGLE
+ ***************************************************/
+
+let sidemenu = document.getElementById("sidemenu");
+
+function openMenu() {
+  sidemenu.style.right = "0";
+}
+
+function closeMenu() {
+  sidemenu.style.right = "-200px";
+}
